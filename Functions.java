32badea5166dea302e5eee51f648394ec6938c36
Ryan Kleinberg <kryan225.gomets@gmail.com>
2018-10-14 17:05:10 -0700

    Add files via upload
    Has updated update functions

--- Functions.java
+++ Functions.java
@@ -32,15 +32,31 @@ public class Functions {
 		//Agent a is the agent to be updated, expression is the expression that the agent's partner expressed
 		//quality is the quality of the interaction ex: positive: 1, negative: -1,
 		//		for neutral expression: expression should be 0, quality has no effect
-		public static void updateAgent(Agent a, int expression, int quality){
-			int exp = expression * quality;//if it is a negative interaction, the -1 value for quality will switch directions of the bias shift
+		public static void updateAgent(Agent a, int partnerExpress, int myExpress, int quality, int learningRate){
+			int exp = partnerExpress * quality;//if it is a negative interaction, the -1 value for quality will switch directions of the bias shift
+			
+			
+			//want to check to see if the agent is expression the opposite of what it's tendency is 
+			//if this is happening we want to change the equation so the outcome is as if they were an agent with an opposite tendency 
+			double rBias = a.getBias();
+			boolean oppExpression = false;
+			oppExpression = !( ((rBias < .5) && (myExpress == 1)) || ((rBias >= .5) && (myExpress == -1)) );
+			
+			if(oppExpression){
+				myExpress = myExpress * -1;
+			}
+			
 			
 			double bias = a.getBias();
 			double comm = a.getCommitment();
+			double neumerator = 1-comm;
 			
-			double newBias = bias - ( ( (1-comm) /10) * exp);
-			//double newComm = comm + ( ( ((1/2) - (bias))/10 ) * exp);
-			double newComm = comm - ( ( (1-comm) /10) * quality);
+			
+			//calculate new values for the bias and commitment
+			double newBias = bias - ( ( neumerator / learningRate) * exp);
+			double newComm = comm + ( ( neumerator / learningRate) * myExpress * exp);
+			
+			//update the bias and commitments
 			a.setBias(newBias);
 			a.setCommitment(newComm);
 			
@@ -54,24 +70,23 @@ public class Functions {
 			int express2 = express(a2);
 			
 			//Both agents agree - Move commitment up
-			if(Math.round(a1.getBias()) == Math.round(a2.getBias())){
-				//a1.setCommitment(a1.getCommitment() + (0.05 * express2));
-				//a2.setCommitment(a2.getCommitment() + (0.05 * express1));
-				updateAgent(a1, express2, -1);
-				updateAgent(a2, express1, -1);
+			if(express1 == express2){
+
+				updateAgent(a1, express2, express1, 1, 10);
+				updateAgent(a2, express1, express2, 1, 10);
 				
 			}else{
 				//Agents don't agree - adjust commitment based off openness
 				double threshold = Math.random();
 				if(threshold < a1.getCommitment()){//agents dont agree and A1 has a bad interaction: reinforce bias + commitment
-					updateAgent(a1, express2, -1);
+					updateAgent(a1, express2, express1, -1, 10);
 				}else{//agents dont agree but A1 has good interaction: decrease bias + commitment
-					updateAgent(a1, express2, 1);
+					updateAgent(a1, express2, express1, 1, 10);
 				}
-				if(threshold < a2.getCommitment()){//agents dont agree and A2 has a bad interaciton: reinforce bias + commitment
-					updateAgent(a2, express1, -1);
+				if(threshold < a2.getCommitment()){//agents dont agree and A2 has a bad interaction: reinforce bias + commitment
+					updateAgent(a2, express1, express2, -1, 10);
 				}else{//agents dont agree but A2 has a good interaction: decrease bias + commitment
-					updateAgent(a2, express1, 1);
+					updateAgent(a2, express1, express2, 1, 10);
 				}
 				
 				
@@ -154,9 +169,15 @@ public class Functions {
 		
 		public static void main(String []args) {
 
-			Agent a = new Agent(.8,.7);
-			updateAgent(a, 0, 1);
-			System.out.println(a.getAgent());
+			Agent a = new Agent(.7,.5);
+			Agent a1 = new Agent(.1, .5);
+			//UpdateAgent(agent, partnerExpr, MyExpr, Qual, LR)
+			for(int i = 0; i < 10; i++){
+				interact(a, a1);
+				System.out.println(a.getAgent());
+				System.out.println(a1.getAgent());
+			}
+			
 			
 		}
 	
